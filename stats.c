0fde642619d58c478c3c117bd4c9b62f26fae618
Aaron Lal <alal022@aucklanduni.ac.nz>
2018-06-10 04:57:08 -0600

    3rd commit: Functioning programming

--- stats.c
+++ stats.c
@@ -28,8 +28,8 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
-
+void main() 
+{
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
@@ -38,43 +38,178 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   unsigned char sorted[SIZE];  // sorted dataset
+  
   /* Statistics and Printing Functions Go Here */
-
+  sort_array(test, SIZE, sorted);  
+  printf("Test data ");
+  print_array(test, SIZE);
+  printf("Sorted data ");
+  print_array(sorted, SIZE);
+  print_statistics(test, SIZE, sorted);
+  
 }
 
 /* Add other Implementation File Code Here */
 
-void print_statistics(unsigned char * ptr, int count)
+void print_statistics(unsigned char * ptrA, int count, unsigned char * ptrB)
 {
-
+  float med; float mean;
+  int max; int min;
+  
+  /* Calculate median */
+  med = find_median(ptrB, count);
+  /* Calculate mean */
+  mean = find_mean(ptrA, count);
+  /* Calculate maximum */
+  max = find_maximum(ptrA, count);
+  /* Calculate minimum */
+  min = find_minimum(ptrA, count);
+  
+  /* Print summary */
+  printf("Median = %f\nMean = %f\nMaximum = %d\nMinimum = %d\n", med, mean, max, min);
+  
+  
 }
 
 void print_array(unsigned char * ptr, int count)
 {
-
+  int i;
+  printf("[");
+  for(i=0; i<count; i++){
+    printf("%d", *ptr);
+    if(i < count-1){printf(", ");}
+    ptr++;
+  }
+  printf("]\n");
 }
- 
+
 float find_median(unsigned char * ptr, int count)
 {
-
+  double a;
+  double b;
+  
+  ptr = ptr + ((count-1)/2);
+  /* Check if element count is odd */
+  if(count%2 == 1)	//return middle element if odd
+  {
+    return *ptr;
+  }
+  else	//return average of 2 middle elements if even
+  {
+    a = *ptr;
+    ptr++;
+    b = *ptr;
+    //printf("midL = %f, midR = %f\n", a, b);	//test output
+    return (a + b)/2;
+  }
 }
- 
+
 float find_mean(unsigned char * ptr, int count)
 {
-
+  float total;
+  int i;
+  for(i = 0; i < count; i++)
+  {
+    total += *ptr;
+    ptr++;
+  }
+  return total/count;
 }
- 
+
 int find_maximum(unsigned char * ptr, int count)
 {
-
+  /* Max from unsorted array */
+  int max = *ptr;
+  int i;
+  for(i = 0; i < count; i++)
+  {
+    if(*ptr > max){max = *ptr;}
+    ptr++;
+  }
+  return max;
+  /* Max from sorted array */
+  /*ptr = ptr + count - 1;
+  return *ptr;*/
 }
- 
+
 int find_minimum(unsigned char * ptr, int count)
 {
-
+  /* Max from unsorted array */
+  int min = *ptr;
+  int i;
+  for(i = 0; i < count; i++)
+  {
+    if(*ptr < min){min = *ptr;}
+    ptr++;
+  }
+  return min;
+  /* Min from sorted array */
+  /*return *ptr;*/
 }
 
 void sort_array(unsigned char * ptrA, int count, unsigned char * ptrB)
 {
+  unsigned char swap[count];
+  unsigned char * ptrC = swap;
 
+  copy_array(ptrA, count, ptrB);
+  copy_array(ptrA, count, ptrC);
+  split(ptrC, 0, count, ptrB);
 }
+
+void copy_array(unsigned char * ptrA, int count, unsigned char * ptrB)
+{
+  int i;
+  for(i = 0; i < count; i++)
+  {
+    *ptrB = *ptrA;
+    ptrA++;
+    ptrB++;
+  }
+}
+
+void split(unsigned char * ptrC, int istart, int iend, unsigned char * ptrB)
+{
+  int imiddle;
+  
+  if(iend - istart < 2){return;}
+  
+  imiddle = (iend + istart) / 2;
+  
+  split(ptrB, istart, imiddle, ptrC);
+  split(ptrB, imiddle, iend, ptrC);
+
+  merge(ptrC, istart, imiddle, iend, ptrB);
+  
+}
+
+void merge(unsigned char * ptrC, int istart, int imiddle, int iend, unsigned char * ptrB)
+{
+
+  unsigned char * ptr_k;
+  unsigned char * ptr_i;
+  unsigned char * ptr_j;
+  int i = istart;
+  int j = imiddle;
+  int k;
+ 
+  // While there are elements in the left or right runs...
+  for (k = istart; k < iend; k++)
+  {
+    ptr_i = ptrC + i;
+    ptr_j = ptrC + j;
+    ptr_k = ptrB + k;
+    // If left run head exists and is >= existing right run head.
+    if (i < imiddle && (j >= iend || *ptr_i >= *ptr_j))
+    {
+      *ptr_k = *ptr_i;
+      i = i + 1;
+    }
+    else 
+    {
+      *ptr_k = *ptr_j;
+      j = j + 1;
+    }
+  }
+}
+
--- stats.h
+++ stats.h
@@ -21,7 +21,7 @@
 
 /* Add Your Declarations and Function Comments here */
 
-void print_statistics(unsigned char * ptr, int count);
+void print_statistics(unsigned char * ptrA, int count, unsigned char * ptrB);
 /**
  * @brief A function that prints the statistics of an array including minimum, maximum, mean, and median.
  *
@@ -39,12 +39,10 @@ void print_array(unsigned char * ptr, int count);
 /**
  * @brief Given an array of data and a length, prints the array to the screen
  *
- * <Add Extended Description Here>
+ * The array is printed as "array_name[element1, element2, ... elementn]
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param * ptr is a pointer to characters in the array being printed
+ * @param count is an integer variable for number of array elements
  *
  * @return void
  */
@@ -54,11 +52,10 @@ float find_median(unsigned char * ptr, int count);
  * @brief Given an array of data and a length, returns the median value
  *
  * If there is an even number of values, the returned value will be the average of the two median values
+ * This function works only with a sorted array
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param * ptr is a pointer to characters in the sorted data array
+ * @param count is an integer variable for number of array elements
  *
  * @return Median as a floating point value
  */
@@ -67,12 +64,10 @@ float find_mean(unsigned char * ptr, int count);
 /**
  * @brief Given an array of data and a length, returns the mean
  *
- * <Add Extended Description Here>
+ * Works by dividing the sum of all elements by the array size
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param * ptr is a pointer to characters in data array being averaged
+ * @param count is an integer variable for number of array elements
  *
  * @return Mean as a floating point value
  */
@@ -81,12 +76,10 @@ int find_maximum(unsigned char * ptr, int count);
 /**
  * @brief Given an array of data and a length, returns the maximum
  *
- * <Add Extended Description Here>
+ * Scans the array comparing each value to highest value encountered
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param * ptr is a pointer to characters in data array being searched
+ * @param count is an integer variable for number of array elements
  *
  * @return Maximum as an integer value
  */
@@ -95,12 +88,10 @@ int find_minimum(unsigned char * ptr, int count);
 /**
  * @brief Given an array of data and a length, returns the minimum
  *
- * <Add Extended Description Here>
+ * Scans the array comparing each value to lowest value encountered
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param * ptr is a pointer to characters in data array being searched
+ * @param count is an integer variable for number of array elements
  *
  * @return Minimum as an integer value
  */
@@ -109,14 +100,65 @@ void sort_array(unsigned char * ptrA, int count, unsigned char * ptrB);
 /**
  * @brief Given an array of data and a length, sorts the array from largest to smallest
  *
- * The zeroth Element should be the largest value, and the last element (n-1) should be the smallest valu
+ * The zeroth element is the largest value, and the last element (n-1) is the smallest value
+ * This function used the merge-sort algorithm which also calls the following functions:
+ * copy_array() and split() which calls the merge() function
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param * ptrA is a pointer to characters in the origninal data array
+ * @param count is an integer variable for number of array elements in dataset
+ * @param * ptrB is a pointer to characters in the array designated for the sorted data
+ * @param swap[] is a local character array for sorting values
+ * @param * ptrC is a local pointer to charactters in the swap array
  *
  * @return void
+ */
+ 
+void copy_array(unsigned char * ptrA, int count, unsigned char * ptrB);
+/**
+ * @brief Copies one array of a given size into another array of equal size
+ *
+ * @param * ptrA is a pointer to characters in the origninal data array
+ * @param count is an integer variable for number of array elements in dataset
+ * @param * ptrB is a pointer to characters in the array designated for the copy
+ *
+ * @return void
+ */
+ 
+void split(unsigned char * ptrC, int istart, int iend, unsigned char * ptrB);
+/**
+ * @brief Split is a recursive function which carries out the sorting using the merge-sort algorithm
+ *
+ * The split funtion splits the array half and then merges the the two halves in order of 
+ * highest to lowest using the merge() function 
+ *
+ * @param * ptrC is a pointer to characters in the swap array 
+ * @param istart is an integer variable for the left bound index of array to be split
+ * @param iend is an integer variable for the right bound index of array to be split 
+ * @param * ptrB is a pointer to characters in the sorted data array
+ * @param imiddle is a local integer variable for the index at which to split the array
+ *
+ * @return void
+ */
+ 
+void merge(unsigned char * ptrC, int istart, int imiddle, int iend, unsigned char * ptrB);
+/**
+ * @brief Merges the split array in order of highest to lowest value
+ *
+ * <Add Extended Description Here>
+ *
+ * @param * ptrC is a pointer to characters in the swap array
+ * @param istart is an integer variable for the left bound index of split array
+ * @param imiddle is an integer variable for the index at which array is split
+ * @param iend is an integer variable for the righ bound index of split array
+ * @param * ptrB is a pointer to characters in the sorted data array
+ * @param * ptr_k is a local pointer to the characters in array being merged
+ * @param * ptr_i is a local pointer to the characters in left portion of swap array
+ * @param * ptr_j is a local pointer to the characters in right portion of swap array
+ * @param i is a local integer index variable for counting through left portion of swap array
+ * @param j is a local integer index variable for counting through right portion of swap array
+ * @param k is a local integer index variable for counting through full portion of sorted array
+ *
+ * @return <void
  */
  
 #endif /* __STATS_H__ */
